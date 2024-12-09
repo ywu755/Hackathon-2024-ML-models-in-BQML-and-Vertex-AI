7425206fe0356ec077d65acddcaa59cc13046280
Karin Wu <62184906+ywu755@users.noreply.github.com>
2024-12-08 22:14:39 -0600

    Create explain_forecast_arima_plus_xreg_model.sql

---
+++ hackathon_bqml_dbt/models/hourly_energy_pjm/explain_forecast_arima_plus_xreg_model.sql
@@ -0,0 +1,13 @@
+{{
+    config(
+        schema='hackathon_q1_2024',
+        materialized='table'
+    )
+}}
+
+
+SELECT 'AEP' as utility, * FROM ({{ explain_arima_plus_xreg_model('AEP') }})
+UNION ALL
+SELECT 'COMED' as utility, * FROM ({{ explain_arima_plus_xreg_model('COMED') }})
+UNION ALL
+SELECT 'DAYTON' as utility, * FROM ({{ explain_arima_plus_xreg_model('DAYTON') }})
