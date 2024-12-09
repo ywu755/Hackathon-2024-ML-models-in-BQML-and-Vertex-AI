9d3602e43b62d9cf2ed29e7b49f86780797f5663
Karin Wu <62184906+ywu755@users.noreply.github.com>
2024-12-08 22:13:29 -0600

    Update evaluate_arima_model.sql

--- hackathon_bqml_dbt/models/hourly_energy_pjm/evaluate_arima_model.sql
+++ hackathon_bqml_dbt/models/hourly_energy_pjm/evaluate_arima_model.sql
@@ -1,27 +1,12 @@
-
 {{
     config(
         schema='hackathon_q1_2024',
-        materialized='model',
-        ml_config={
-            'model_type': 'ARIMA_PLUS',
-            'time_series_timestamp_col' : 'datetime',
-            'time_series_id_col' : 'utility',
-            'time_series_data_col' : 'demand',
-            'auto_arima_max_order' : 5
-        }
+        materialized='table'
     )
 }}
 
-SELECT
-  `Datetime` AS `datetime`,
-  utility,
-  SAFE_CAST(demand AS FLOAT64) AS demand
-FROM `kv-ds-lder-der.hackathon_q1_2024.hourly_energy_pjm`
-UNPIVOT (
-  demand FOR utility IN (
-    AEP, COMED, DAYTON, DEOK, DOM, DUQ, EKPC, FE, NI, PJME, PJMW
-  )
+SELECT *
+FROM ML.ARIMA_EVALUATE(
+    MODEL {{ ref('arima_model') }},
+    STRUCT(TRUE AS show_all_candidate_models)
 )
-WHERE Datetime > '2016-01-01 00:00:00'
-ORDER BY datetime
